8b38b83700e6210d2c0bd26d9bc479bd4aa8f8f5
yyyhah <34295390+yyyhah@users.noreply.github.com>
2019-04-30 20:17:59 +0800

    Update ParseTorrent.java

--- TorrentDownload/ParseTorrent.java
+++ TorrentDownload/ParseTorrent.java
@@ -19,9 +19,9 @@ import org.jsoup.Connection.Response;
 
 import Connection.UDPConnection;
 import net.sf.json.JSONObject;
-//¸Ã³ÌĞòÎ´¿¼ÂÇÀıÈçi23e4:adsfÕâÖÖÓï·¨µÄ¿ÉÄÜĞÔ¡£ÒòÎª¶ÔÓÚÖÖ×Ó£¬Ò»°ãÊÇÒ»¶Î×Öµä±àÂë¡£²»»á³öÏÖÒÔÉÏÇé¿ö¡£³öÏÖÒÔÉÏÇé¿öÔò½âÎö³ö´í
+//è¯¥ç¨‹åºæœªè€ƒè™‘ä¾‹å¦‚i23e4:adsfè¿™ç§è¯­æ³•çš„å¯èƒ½æ€§ã€‚å› ä¸ºå¯¹äºç§å­ï¼Œä¸€èˆ¬æ˜¯ä¸€æ®µå­—å…¸ç¼–ç ã€‚ä¸ä¼šå‡ºç°ä»¥ä¸Šæƒ…å†µã€‚å‡ºç°ä»¥ä¸Šæƒ…å†µåˆ™è§£æå‡ºé”™
 public class ParseTorrent {
-	//bencodeµÄÔ¤²â·ÖÎö±í
+	//bencodeçš„é¢„æµ‹åˆ†æè¡¨
 	private static JSONObject grammer = new JSONObject();
 	private static String[][] grammerArray = {
 			{"iMe","lAe","dCe","N:S","null","null","null","null","null","null","null"},
@@ -36,7 +36,7 @@ public class ParseTorrent {
 	private static Character[] terminal = {'i','l','d','D','c','$','-','0','z',':','e'};
 	
 	private static String result2 = "";
-	private static Stack<Character> result = new Stack<Character>();//Í¨¹ı½«d£¬l£¬iÕâÈıÀàÊı¾İÑ¹Õ»£¬³öÕ»µÄ·½·¨£¬ÊµÏÖ±ã·ÖÎö£¬±ß±àÒë
+	private static Stack<Character> result = new Stack<Character>();//é€šè¿‡å°†dï¼Œlï¼Œiè¿™ä¸‰ç±»æ•°æ®å‹æ ˆï¼Œå‡ºæ ˆçš„æ–¹æ³•ï¼Œå®ç°ä¾¿åˆ†æï¼Œè¾¹ç¼–è¯‘
 	private static Character[] noTerminal = {'E','N','M','A','B','C','G','F','S'};
 	static {
 		for(int nt=0;nt<noTerminal.length;nt++) {
@@ -45,12 +45,12 @@ public class ParseTorrent {
 			}
 		}
 	}
-	private static int StringIndex = -1;//×Ö·û´®ÀàĞÍ½áÎ²µÄÏÂ±ê
-	private static Boolean isZero = true;//ÅĞ¶ÏÊı¾İÊÇ·ñÎª0
+	private static int StringIndex = -1;//å­—ç¬¦ä¸²ç±»å‹ç»“å°¾çš„ä¸‹æ ‡
+	private static Boolean isZero = true;//åˆ¤æ–­æ•°æ®æ˜¯å¦ä¸º0
 	private static List<Character> noTerminaList = Arrays.asList(noTerminal);
-	//Æ¥Åä×Ö·ûµÄÕæÊµÀàĞÍ
+	//åŒ¹é…å­—ç¬¦çš„çœŸå®ç±»å‹
 	private static Character getType(char s,int index) {
-		//Õâ¸öÊÇÅĞ¶Ïµ±Ç°×Ö·ûÊÇ·ñÊôÓÚ×Ö·û´®
+		//è¿™ä¸ªæ˜¯åˆ¤æ–­å½“å‰å­—ç¬¦æ˜¯å¦å±äºå­—ç¬¦ä¸²
 		if(index < StringIndex) {
 			isZero = true;
 			return 'c';
@@ -59,7 +59,7 @@ public class ParseTorrent {
 			return 'z';
 		}
 		if(Character.isDigit(s)) {
-			//Èç¹ûÎª0-9µÄÊı×ÖÄÇÃ´·µ»ØDÀàĞÍ
+			//å¦‚æœä¸º0-9çš„æ•°å­—é‚£ä¹ˆè¿”å›Dç±»å‹
 			int value = s-48;
 			if(!isZero) {
 				return 'D';
@@ -77,55 +77,55 @@ public class ParseTorrent {
 	}
 	public static String BDecode(String text) {
 		char[] textArray = text.toCharArray();
-		int listenState = 0;//µ±µ¯³öN:µÄÊ±ºò¿ªÊ¼¼àÌıºó·½µÄ×Ö·û´®³¤¶È,1±íÊ¾×¼±¸¼àÌı£¬2±íÊ¾¿ªÊ¼¼àÌı£¬0±íÊ¾²»¼àÌı
+		int listenState = 0;//å½“å¼¹å‡ºN:çš„æ—¶å€™å¼€å§‹ç›‘å¬åæ–¹çš„å­—ç¬¦ä¸²é•¿åº¦,1è¡¨ç¤ºå‡†å¤‡ç›‘å¬ï¼Œ2è¡¨ç¤ºå¼€å§‹ç›‘å¬ï¼Œ0è¡¨ç¤ºä¸ç›‘å¬
 		int index = 0;
 		String result1 = "";
 		Stack<Character> stack = new Stack<Character>();
-		String stringLength = "";//±£´æ×Ö·û´®³¤¶È
+		String stringLength = "";//ä¿å­˜å­—ç¬¦ä¸²é•¿åº¦
 		stack.add('$');
 		stack.add('E');
-		//ÈôÕ»¶¥·Ç¿Õ
+		//è‹¥æ ˆé¡¶éç©º
 		while(stack.peek() != '$') {
 			char top = stack.peek();
 			char indexChar = getType(textArray[index],index);
-			//Èç¹ûÊÇÖÕ½á·û
+			//å¦‚æœæ˜¯ç»ˆç»“ç¬¦
 			if(!noTerminaList.contains(top)) {
-				//Èç¹ûÕ»¶¥×Ö·ûÎª¿Õ£¬µ¯³ö¿Õ×Ö·û
+				//å¦‚æœæ ˆé¡¶å­—ç¬¦ä¸ºç©ºï¼Œå¼¹å‡ºç©ºå­—ç¬¦
 				if(top == '@') {
 					stack.pop();
 				}
-				//Èç¹û¸ÃÖÕ½á·ûºÍµ±Ç°¶ÁÈ¡×Ö·ûÒ»ÖÂ,¶ÁÈ¡ÏÂÒ»¸ö×Ö·û£¬½«Õ»¶¥µ¯³ö,²»Ò»ÖÂÔò½âÎö³ö´í
+				//å¦‚æœè¯¥ç»ˆç»“ç¬¦å’Œå½“å‰è¯»å–å­—ç¬¦ä¸€è‡´,è¯»å–ä¸‹ä¸€ä¸ªå­—ç¬¦ï¼Œå°†æ ˆé¡¶å¼¹å‡º,ä¸ä¸€è‡´åˆ™è§£æå‡ºé”™
 				else if(top == indexChar) {
 					index += 1;
 					char remove = stack.pop();
 					result2 += remove;
 					if(remove=='d') {
-						result1 += "£¤{";
+						result1 += "ï¿¥{";
 						result.push('d');
 					}else if(remove=='l') {
 						result.push('l');
-						result1 += "£¤[";
+						result1 += "ï¿¥[";
 					}else if(remove=='i') {
 						result.push('i');
-						result1 += '£¤';
+						result1 += 'ï¿¥';
 					}else if(remove==':') {
 						result1 += ":";
 					}else if(remove=='e') {
 						char type = result.pop();
 						if(type == 'i') {
-							result1 += '£¤';
+							result1 += 'ï¿¥';
 						}if(type == 'd') {
-							result1 += "}£¤";
+							result1 += "}ï¿¥";
 						}else if(type=='l') {
-							result1 += "]£¤";
+							result1 += "]ï¿¥";
 						}
 					}else if(remove=='z') {
-						result1 += textArray[index-1]+"£¤";
+						result1 += textArray[index-1]+"ï¿¥";
 					}else {
 						result1 += textArray[index-1];
 					}
 					if(remove == ':' && listenState==1) {
-						//µ±¶Áµ½:£¬½áÊø¼àÌı,¼ÆËãµÃµ½µÄ×Ö·û´®³¤¶È
+						//å½“è¯»åˆ°:ï¼Œç»“æŸç›‘å¬,è®¡ç®—å¾—åˆ°çš„å­—ç¬¦ä¸²é•¿åº¦
 						StringIndex = index + Integer.valueOf(stringLength)-1;
 						stringLength = "";
 						listenState = 0;	
@@ -139,7 +139,7 @@ public class ParseTorrent {
 					return null;
 				}
 			}else {
-				//Èç¹ûµ±Ç°Õ»¶¥ÊÇ·ÇÖÕ½á·û,Èç¹û²»´æÔÚ¡£´Ó·ÇÖÕ½á·ûµ½µ±Ç°×Ö·ûµÄÓ³Éä£¬Ôò½âÎö³ö´í£¬Èç¹û´æÔÚÓ³Éä£¬½«Õ»¶¥µ¯³ö£¬½«ĞÂµÄ²úÉúÊ½Ñ¹ÈëÕ»
+				//å¦‚æœå½“å‰æ ˆé¡¶æ˜¯éç»ˆç»“ç¬¦,å¦‚æœä¸å­˜åœ¨ã€‚ä»éç»ˆç»“ç¬¦åˆ°å½“å‰å­—ç¬¦çš„æ˜ å°„ï¼Œåˆ™è§£æå‡ºé”™ï¼Œå¦‚æœå­˜åœ¨æ˜ å°„ï¼Œå°†æ ˆé¡¶å¼¹å‡ºï¼Œå°†æ–°çš„äº§ç”Ÿå¼å‹å…¥æ ˆ
 				String tempStr = "";
 				String grammerTemp = null;
 				tempStr += top;
@@ -178,7 +178,7 @@ public class ParseTorrent {
 		String result1 = "";
 		result1 = BDecode(text);
 		if(result1==null) {
-			System.out.println("½âÎö³ö´í!");
+			System.out.println("è§£æå‡ºé”™!");
 			return null;
 		}		
 		return result1;
@@ -199,16 +199,16 @@ public class ParseTorrent {
 					}
 					else if(j<3&&j>0) {
 						ip1 += ".";
-						ip1 += bytes[j]& 0xff;//ÎŞ·ûºÅ¶ş½øÖÆ×ª10½øÖÆ
+						ip1 += bytes[j]& 0xff;//æ— ç¬¦å·äºŒè¿›åˆ¶è½¬10è¿›åˆ¶
 					}
 					else if(j==3) {
 						ip1 += ".";
-						ip1 += bytes[j]& 0xff;//ÎŞ·ûºÅ¶ş½øÖÆ×ª10½øÖÆ
+						ip1 += bytes[j]& 0xff;//æ— ç¬¦å·äºŒè¿›åˆ¶è½¬10è¿›åˆ¶
 						ip1 += ":";
 					}else {
 						int k = (bytes[j]& 0xff);
 						int y = (bytes[j+1]&0xff);
-						ip1 += (bytes[j]& 0xff)*128+(bytes[j+1]&0xff);//ÎŞ·ûºÅ¶ş½øÖÆ×ª10½øÖÆ
+						ip1 += (bytes[j]& 0xff)*128+(bytes[j+1]&0xff);//æ— ç¬¦å·äºŒè¿›åˆ¶è½¬10è¿›åˆ¶
 						break;
 					}
 						
@@ -220,33 +220,35 @@ public class ParseTorrent {
 		}
 		return torrentJson;
 	}
+//è§£æç§å­ç¤ºä¾‹
 //	public static void main(String args[]) throws IOException{
 //		String hash = "cfbc6db371632c90439d99c7a7b8bba530d9e54c";
-//		//ÏÂÔØÖÖ×ÓÎÄ¼ş
+//		//ä¸‹è½½ç§å­æ–‡ä»¶
 //		byte[] getTorrent = GetTorrent.downTorrent(hash);
-//		//½«ÖÖ×ÓÎÄ¼ş´æÈë±¾µØ
+//		//å°†ç§å­æ–‡ä»¶å­˜å…¥æœ¬åœ°
 //		RWDTorrent.download(getTorrent, "E:/test3.torrent");
-//		//´Ó±¾µØ¶ÁÈ¡ÖÖ×ÓÎÄ¼ş
+//		//ä»æœ¬åœ°è¯»å–ç§å­æ–‡ä»¶
 //		char[] torrentChar = RWDTorrent.read("E:/test.torrent");
-//		//char[] torrentChar = RWDTorrent.readFromGet(getTorrent);
 //		if(torrentChar==null) {
-//			System.out.println("¸ÃÎÄ¼ş²»´æÔÚ");
+//			System.out.println("è¯¥æ–‡ä»¶ä¸å­˜åœ¨");
 //			return;
 //		}
 //		String torrent = new String(torrentChar);
-//		//½âÎöÖÖ×Ó
+//		//è§£æç§å­
 //		String result1 = ParseBencode(torrent);
 //		if(result1==null) {
-//			System.out.println("ÎÄ¼ş½âÎö³ö´í!");
+//			System.out.println("æ–‡ä»¶è§£æå‡ºé”™!");
 //		}else {
-//			System.out.println("ÎÄ¼ş½âÎö³É¹¦!");
+//			System.out.println("æ–‡ä»¶è§£ææˆåŠŸ!");
 //		}
-//		//ÏÔÊ¾ÖÖ×Ó½á¹¹
+//		//æ˜¾ç¤ºç§å­ç»“æ„
 //		JSONObject torrentJson = showTorrent(result1);
-//		//½«½âÎöºóµÄÖÖ×ÓÎÄ¼şĞ´ÈëÎÄ¼ş
+//		//å°†è§£æåçš„ç§å­æ–‡ä»¶å†™å…¥æ–‡ä»¶
 //		RWDTorrent.write(Regex.cleanData(result1),"E:/temp3.txt");
-		//·¢ËÍUDP
+		//å‘é€UDP
 //	}
+	
+//è°ƒç”¨bè§£ç ç¤ºä¾‹
 //	public static void main(String[] args) {
 //		UDPConnection send_conn = new UDPConnection();
 //		UDPConnection receive_conn = new UDPConnection(12700);
@@ -258,13 +260,13 @@ public class ParseTorrent {
 	public static String bencode(JSONObject obj) {
 		String result = "";
 		String str = obj.toString();
-		//±ê¼Ç»ØÌî£¬Ö÷ÒªÊÇÕë¶Ô×Ö·û´®³¤¶È»ØÌî
+		//æ ‡è®°å›å¡«ï¼Œä¸»è¦æ˜¯é’ˆå¯¹å­—ç¬¦ä¸²é•¿åº¦å›å¡«
 		int Rindex = -1;
 		int index = 0;
 		char top = '!';
 		Boolean isStr = false;
 		Boolean isInt = false;
-		int lenght = 0 ;//±íÊ¾×Ö·û´®µÄ³¤¶È
+		int lenght = 0 ;//è¡¨ç¤ºå­—ç¬¦ä¸²çš„é•¿åº¦
 		char bchar[] = str.toCharArray();
 		for(char c: bchar) {
 			if(c=='{') {
@@ -276,7 +278,7 @@ public class ParseTorrent {
 				result += 'l';
 				index += 1;
 			}else if(c=='"') {
-				//»ØÌî
+				//å›å¡«
 				if(isStr) {
 					String num = String.valueOf(lenght);
 					result = result.substring(0,Rindex)+num+":"+result.substring(Rindex);
@@ -326,6 +328,7 @@ public class ParseTorrent {
 		}
 		return out;
 	}
+//è°ƒç”¨bç¼–ç ç¤ºä¾‹
 //	public static void main(String[] args) {
 //		String code = "{\"t\":\"tx1\", \"y\":\"q\", \"q\":\"ping\", \"a\":{\"id\":\"abcdefghij0123456789\"}}";
 //		//System.out.println(bencode(JSONObject.fromObject(code)));
